ee53667d2d0192d495f71fc7e22ed4a90a2f988c
SoYeon <ksy2710@hanmail.net>
2022-01-27 18:28:01 +0900

    단계 2. 상품 카테고리 메뉴 출력 및 사용자 입력 값 처리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,40 @@ fun main(args: Array<String>) {
 
     val name = readLine() // 화면에서 사용자 입력받기 함수
 
-    println("""
+    println(
+        """
         감사합니다. 반갑습니다. $name 님
         원하시는 카테고리를 입력해주세요.
         ***=============================***
-    """.trimIndent())
+    """.trimIndent()
+    )
 
+    /*
+     * (1) 상품 카테고리 표시
+     * (2) 사용자 입력받기
+     * (3) 사용자가 잘못된 값 입력 처리
+     */
+
+    val categories = arrayOf("패션", "전자기기", "반려동물용품")
+    for (category in categories) {
+        println(category)
+    }
+    println("==> 장바구니로 이동하시려면 '#'을 입력해주세요.")
+
+    // 카테고리 입력받기
+    var selectedCategory = readLine()
+
+    // 사용자 올바른 값을 입력할때까지 반복처리
+    while (selectedCategory.isNullOrBlank()) { // null이나 빈공간이 없을때까지
+        println("값을 입력해주세요!")
+        selectedCategory = readLine()
+    }
+
+    if (selectedCategory == "#") {
+        // TODO (1) 장바구니 이동
+
+    } else {
+        // TODO (2) 카테고리 상품 목록 보여주기
+        // TODO (3) 카테고리 목록에 없는 경우 처리
+    }
 }
