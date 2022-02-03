cc1f32732bf10b3fbc4ee4d8a7eb09763b12b228
SoYeon <ksy2710@hanmail.net>
2022-02-03 10:53:41 +0900

    단계 3-1. Home화면과  Category화면 분리 및  Product 클래스 추가 후  상품목록 출력

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,45 @@
+package screen
+
+class ShoppingCategory {
+    fun showCategories() {
+        /*
+         * (1) 상품 카테고리 표시
+         * (2) 사용자 입력받기
+         * (3) 사용자가 잘못된 값 입력 처리
+         */
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println(category)
+        }
+        println("==> 장바구니로 이동하시려면 '#'을 입력해주세요.")
+
+        // 카테고리 입력받기
+        var selectedCategory = readLine()
+
+        // 사용자 올바른 값을 입력할때까지 반복처리
+        while (selectedCategory.isNullOrBlank()) { // null이나 빈공간이 없을때까지
+            println("값을 입력해주세요!")
+            selectedCategory = readLine()
+        }
+
+        if (selectedCategory == "#") {
+            // TODO (1) 장바구니 이동
+
+        } else {
+            if (categories.contains(selectedCategory)) {
+                // 카테고리 상품목록 보여주기 --- 있으면 selectedCategory 보여주기
+                val shoppingProductList = ShoppingProductList() // 객체 생성
+                shoppingProductList.showProducts(selectedCategory)
+
+            } else {
+                // 카테고리 목록에 없을경우
+                showErrorMessage(selectedCategory)
+            }
+        }
+    }
+
+    private fun showErrorMessage(selectedCategory: String?) {
+        println("[$selectedCategory] : 존재하지 않는 카테고리입니다. 다시 입력해주세요.")
+        showCategories()
+    }
+}
